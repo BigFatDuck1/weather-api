30a5422892833382b3007546f37e764cb418ae05
BigFatDuck1 <clementho999@gmail.com>
2024-03-30 01:40:26 +0800

    Able to pass search term into search function and return result from API

--- dist/main.js
+++ dist/main.js
@@ -114,9 +114,9 @@ eval("\r\n\r\n/* istanbul ignore next  */\r\nfunction styleTagTransform(css, sty
 /*!**********************!*\
   !*** ./src/index.js ***!
   \**********************/
-/***/ ((module, __webpack_exports__, __webpack_require__) => {
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.a(module, async (__webpack_handle_async_dependencies__, __webpack_async_result__) => { try {\n__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _search__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./search */ \"./src/search.js\");\n\r\n\r\n\r\nlet searchterm;\r\n\r\ndocument.querySelector(\"#searchbutton\").addEventListener(\"click\", () => {\r\n    let searchbar = document.querySelector(\"#searchbar\").value;\r\n\r\n    searchterm = searchbar.toLowerCase(); \r\n    searchterm = searchterm.replace(/\\s/g, ''); //Store searched value\r\n    document.querySelector(\"#searchbar\").value = \"\"; //Clear searchbar\r\n    console.log(searchterm);\r\n})\r\n\r\nlet result = await (0,_search__WEBPACK_IMPORTED_MODULE_1__.searchWeather)(\"hongkong\"); //Await works at the top level of a module\r\n\r\ndocument.querySelector(\".results\").textContent = result.location.name;\n__webpack_async_result__();\n} catch(e) { __webpack_async_result__(e); } }, 1);\n\n//# sourceURL=webpack://webpack-template/./src/index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _style_css__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./style.css */ \"./src/style.css\");\n/* harmony import */ var _search__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./search */ \"./src/search.js\");\n\r\n\r\n\r\nlet searchterm;\r\n\r\n//Search on button press\r\ndocument.querySelector(\"#searchbutton\").addEventListener(\"click\", () => {\r\n    onSearch();\r\n})\r\n\r\n//Search on enter key press\r\nwindow.addEventListener(\"keydown\", (event) => {\r\n    if (event.key === \"Enter\" && document.querySelector(\"#searchbar\").value != \"\") {\r\n        onSearch();\r\n    };\r\n})\r\n\r\nasync function onSearch() {\r\n    let searchbar = document.querySelector(\"#searchbar\").value;\r\n\r\n    searchterm = searchbar.toLowerCase(); \r\n    searchterm = searchterm.replace(/\\s/g, ''); //Store searched value\r\n    document.querySelector(\"#searchbar\").value = \"\"; //Clear searchbar\r\n    console.log(searchterm);\r\n\r\n    try {\r\n        let result = await (0,_search__WEBPACK_IMPORTED_MODULE_1__.searchWeather)(searchterm); \r\n        console.log(result);\r\n        document.querySelector(\".results\").textContent = result.location.name;\r\n    }\r\n    catch (error) {\r\n        document.querySelector(\".results\").textContent = \"City not found\";\r\n    }\r\n\r\n}\r\n\r\n// let result = await searchWeather(\"hongkong\"); //Await works at the top level of a module\r\n\r\n// document.querySelector(\".results\").textContent = result.location.name;\n\n//# sourceURL=webpack://webpack-template/./src/index.js?");
 
 /***/ }),
 
@@ -157,75 +157,6 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/async module */
-/******/ 	(() => {
-/******/ 		var webpackQueues = typeof Symbol === "function" ? Symbol("webpack queues") : "__webpack_queues__";
-/******/ 		var webpackExports = typeof Symbol === "function" ? Symbol("webpack exports") : "__webpack_exports__";
-/******/ 		var webpackError = typeof Symbol === "function" ? Symbol("webpack error") : "__webpack_error__";
-/******/ 		var resolveQueue = (queue) => {
-/******/ 			if(queue && queue.d < 1) {
-/******/ 				queue.d = 1;
-/******/ 				queue.forEach((fn) => (fn.r--));
-/******/ 				queue.forEach((fn) => (fn.r-- ? fn.r++ : fn()));
-/******/ 			}
-/******/ 		}
-/******/ 		var wrapDeps = (deps) => (deps.map((dep) => {
-/******/ 			if(dep !== null && typeof dep === "object") {
-/******/ 				if(dep[webpackQueues]) return dep;
-/******/ 				if(dep.then) {
-/******/ 					var queue = [];
-/******/ 					queue.d = 0;
-/******/ 					dep.then((r) => {
-/******/ 						obj[webpackExports] = r;
-/******/ 						resolveQueue(queue);
-/******/ 					}, (e) => {
-/******/ 						obj[webpackError] = e;
-/******/ 						resolveQueue(queue);
-/******/ 					});
-/******/ 					var obj = {};
-/******/ 					obj[webpackQueues] = (fn) => (fn(queue));
-/******/ 					return obj;
-/******/ 				}
-/******/ 			}
-/******/ 			var ret = {};
-/******/ 			ret[webpackQueues] = x => {};
-/******/ 			ret[webpackExports] = dep;
-/******/ 			return ret;
-/******/ 		}));
-/******/ 		__webpack_require__.a = (module, body, hasAwait) => {
-/******/ 			var queue;
-/******/ 			hasAwait && ((queue = []).d = -1);
-/******/ 			var depQueues = new Set();
-/******/ 			var exports = module.exports;
-/******/ 			var currentDeps;
-/******/ 			var outerResolve;
-/******/ 			var reject;
-/******/ 			var promise = new Promise((resolve, rej) => {
-/******/ 				reject = rej;
-/******/ 				outerResolve = resolve;
-/******/ 			});
-/******/ 			promise[webpackExports] = exports;
-/******/ 			promise[webpackQueues] = (fn) => (queue && fn(queue), depQueues.forEach(fn), promise["catch"](x => {}));
-/******/ 			module.exports = promise;
-/******/ 			body((deps) => {
-/******/ 				currentDeps = wrapDeps(deps);
-/******/ 				var fn;
-/******/ 				var getResult = () => (currentDeps.map((d) => {
-/******/ 					if(d[webpackError]) throw d[webpackError];
-/******/ 					return d[webpackExports];
-/******/ 				}))
-/******/ 				var promise = new Promise((resolve) => {
-/******/ 					fn = () => (resolve(getResult));
-/******/ 					fn.r = 0;
-/******/ 					var fnQueue = (q) => (q !== queue && !depQueues.has(q) && (depQueues.add(q), q && !q.d && (fn.r++, q.push(fn))));
-/******/ 					currentDeps.map((dep) => (dep[webpackQueues](fnQueue)));
-/******/ 				});
-/******/ 				return fn.r ? promise : getResult();
-/******/ 			}, (err) => ((err ? reject(promise[webpackError] = err) : outerResolve(exports)), resolveQueue(queue)));
-/******/ 			queue && queue.d < 0 && (queue.d = 0);
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat get default export */
 /******/ 	(() => {
 /******/ 		// getDefaultExport function for compatibility with non-harmony modules
--- src/index.js
+++ src/index.js
@@ -3,15 +3,37 @@ import { searchWeather } from "./search";
 
 let searchterm;
 
+//Search on button press
 document.querySelector("#searchbutton").addEventListener("click", () => {
+    onSearch();
+})
+
+//Search on enter key press
+window.addEventListener("keydown", (event) => {
+    if (event.key === "Enter" && document.querySelector("#searchbar").value != "") {
+        onSearch();
+    };
+})
+
+async function onSearch() {
     let searchbar = document.querySelector("#searchbar").value;
 
     searchterm = searchbar.toLowerCase(); 
     searchterm = searchterm.replace(/\s/g, ''); //Store searched value
     document.querySelector("#searchbar").value = ""; //Clear searchbar
     console.log(searchterm);
-})
 
-let result = await searchWeather("hongkong"); //Await works at the top level of a module
+    try {
+        let result = await searchWeather(searchterm); 
+        console.log(result);
+        document.querySelector(".results").textContent = result.location.name;
+    }
+    catch (error) {
+        document.querySelector(".results").textContent = "City not found";
+    }
 
-document.querySelector(".results").textContent = result.location.name;
+}
+
+// let result = await searchWeather("hongkong"); //Await works at the top level of a module
+
+// document.querySelector(".results").textContent = result.location.name;
