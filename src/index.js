ff7cf19d9062a7a9d66224bbec59dd530c0fec06
BigFatDuck1 <clementho999@gmail.com>
2024-03-30 01:22:50 +0800

    Added search bar and can store search term

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import "./style.css";
 import { searchWeather } from "./search";
 
+let searchterm;
+
+document.querySelector("#searchbutton").addEventListener("click", () => {
+    let searchbar = document.querySelector("#searchbar").value;
+
+    searchterm = searchbar.toLowerCase(); 
+    searchterm = searchterm.replace(/\s/g, ''); //Store searched value
+    document.querySelector("#searchbar").value = ""; //Clear searchbar
+    console.log(searchterm);
+})
+
 let result = await searchWeather("hongkong"); //Await works at the top level of a module
 
-
-document.querySelector("h1").textContent = result.location.name;
+document.querySelector(".results").textContent = result.location.name;
